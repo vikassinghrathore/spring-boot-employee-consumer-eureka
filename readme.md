ceab529e304bd3d3acfb8aca5ac37e12b2315c9c
vikassinghrathore <vikaskanvika@gmail.com>
2023-03-31 16:45:56 +0530

    consumer eureka

---
+++ readme.md
@@ -0,0 +1,4 @@
+### consumer and producer registered on eureka server
+![img.png](img.png)
+### Consoles
+![img_1.png](img_1.png)
---
+++ src/main/java/com/vks/consumer/SpringBootHelloWorldApplication.java
@@ -0,0 +1,30 @@
+package com.vks.consumer;
+
+import java.io.IOException;
+
+import com.vks.consumer.controller.ConsumerControllerClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClientException;
+
+
+@SpringBootApplication
+public class SpringBootHelloWorldApplication {
+
+	public static void main(String[] args) throws RestClientException, IOException {
+		ApplicationContext ctx = SpringApplication.run(
+				SpringBootHelloWorldApplication.class, args);
+		
+		ConsumerControllerClient consumerControllerClient=ctx.getBean(ConsumerControllerClient.class);
+		System.out.println(consumerControllerClient);
+		consumerControllerClient.getEmployee();
+		
+	}
+	@Bean
+	public  ConsumerControllerClient  consumerControllerClient()
+	{
+		return  new ConsumerControllerClient();
+	}
+}
